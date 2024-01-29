96006459ac8145de16a7d03171d36d4ebaf7273a
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-01-29 14:20:21 +0800

    Initial Commit

---
+++ WYCICO3/WYCICO3App.swift
@@ -0,0 +1,17 @@
+//
+//  WYCICO3App.swift
+//  WYCICO3
+//
+//  Created by wy on 2024/1/29.
+//
+
+import SwiftUI
+
+@main
+struct WYCICO3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
